430c2098a3be2d46cca567b464326da2418b8c22
Abass Zakari <developer.meltwater@gmail.com>
2014-03-23 00:29:29 -0700

    added a site layout

--- spec/controllers/pages_controller_spec.rb
+++ spec/controllers/pages_controller_spec.rb
@@ -1,11 +1,22 @@
 require 'spec_helper'
 
 describe PagesController do
+  render_views
 
   describe "GET 'home'" do
     it "returns http success" do
       get 'home'
       response.should be_success
+    end
+
+    it "should have the title 'Home'" do
+      visit '/pages/home'
+      expect(page).to have_title("Our mission is alive | Home")
+    end
+
+    it "should have a non-blank body" do
+      get 'home'
+      response.body.should_not =~ /<body>\s*<\/body>/
     end
   end
 
@@ -14,6 +25,23 @@ describe PagesController do
       get 'contact'
       response.should be_success
     end
+
+    it "should have the title 'Contact'" do
+      visit '/pages/contact'
+      expect(page).to have_title("Our mission is alive | Contact")
+    end
   end
+
+  describe "GET 'about'" do
+    it "returns http success" do
+      get 'about'
+      response.should be_success
+    end
+  end
+
+  it "should have the title 'About Us'" do
+      visit '/pages/about'
+      expect(page).to have_title("Our mission is alive | About Us")
+    end
 
 end
